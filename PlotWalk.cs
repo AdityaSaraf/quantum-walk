ecb968789ab92a31b9f5c1d884bc6e0c02f74a66
Aditya Saraf <sarafa@cs.uw.edu>
2019-03-20 17:52:11 -0700

    added classical walk

--- PlotWalk.cs
+++ PlotWalk.cs
@@ -55,6 +55,41 @@ namespace Quantum.Walk
                     Console.WriteLine(String.Join(";" , dict));
                 }
             }
+
+            for (int i = 0; i <= numSteps; i++) {
+                Dictionary<double, double> dict = ClassicalRandomWalk(numSteps, numBits);
+                Console.WriteLine(String.Join(";" , dict));
+            }
+        }
+
+        static Dictionary<double, double> ClassicalRandomWalk(int numSteps, int numBits) {
+            double startPos = Math.Pow(2, numBits - 1);
+            double currPos;
+            Dictionary<double, double> probDict = new Dictionary<double, double>();
+            Dictionary<double, int> countDict = new Dictionary<double, int>();
+            Random rand = new Random();
+            int numTrials = 1000000;
+            for (int i = 0; i < Math.Pow(2, numBits); i++) {
+                probDict[i] = 0;
+            }
+            for (int i = 0; i < numTrials; i++) {
+                currPos = startPos;
+                for (int j = 0; j < numSteps; j++) {
+                    if (rand.NextDouble() > 0.5) {
+                        currPos++;
+                    } else {
+                        currPos--;
+                    }
+                }
+                if (!countDict.ContainsKey(currPos)) {
+                    countDict[currPos] = 0;
+                }
+                countDict[currPos]++;
+            }
+            foreach (var num in countDict.Keys) {
+                probDict[num] = (double)countDict[num]/numTrials;
+            }
+            return probDict;
         }
 
         static Dictionary<double, double> ParseDumpFile(String[] dump) {
